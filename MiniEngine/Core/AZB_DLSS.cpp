0e7dea7b40eac8bd76533cd080b220c7b4e16fab
Aliyaan <93915104+AliyaanZBH@users.noreply.github.com>
2024-10-21 17:29:23 +0100

    Cleaned up and improved error feedback by telling user exact driver version needed

--- MiniEngine/Core/AZB_DLSS.cpp
+++ MiniEngine/Core/AZB_DLSS.cpp
@@ -39,7 +39,8 @@ void DLSS::Init(ID3D12Device* device, IDXGIAdapter* Adapter)
 	// using other SDK entry points (i.e. not enough device memory may be available)
 	NVSDK_NGX_Result ret = NVSDK_NGX_D3D12_GetFeatureRequirements(Adapter, &FeatureDiscoveryInfo, &OutSupported);
 
-	if (ret == NVSDK_NGX_Result::NVSDK_NGX_Result_Success)
+	// Assert that ret != fail
+	if (NVSDK_NGX_SUCCEED(ret))
 		// Init NGX!
 		NVSDK_NGX_D3D12_Init(12345678910112021, appDataPath, device);
 	else
@@ -56,43 +57,41 @@ void DLSS::Init(ID3D12Device* device, IDXGIAdapter* Adapter)
 	NVSDK_NGX_Parameter* Params = nullptr;
 	NVSDK_NGX_Result Result = NVSDK_NGX_D3D12_GetCapabilityParameters(&Params);
 
+	// Secondary hardware query
+	NVSDK_NGX_Result ResultDlssSupported = Params->Get(NVSDK_NGX_Parameter_SuperSampling_Available, &DLSS_Supported);
+	if (NVSDK_NGX_FAILED(ResultDlssSupported) || !DLSS_Supported)
+		Utility::Print("\nNVIDIA DLSS not available on this hardware\n\n");
+
+	
+	// Check if the software is at the minimum version for running DLSS, and if it isn't find out what version is needed to help the user get the right one
 	NVSDK_NGX_Result resultUpdatedDriver = Params->Get(NVSDK_NGX_Parameter_SuperSampling_NeedsUpdatedDriver, &needsUpdatedDriver);
 	NVSDK_NGX_Result resultMinDriverVersionMajor = Params->Get(NVSDK_NGX_Parameter_SuperSampling_MinDriverVersionMajor, &minDriverVersionMajor);
 	NVSDK_NGX_Result resultMinDriverVersionMinor = Params->Get(NVSDK_NGX_Parameter_SuperSampling_MinDriverVersionMinor, &minDriverVersionMinor);
 
+	// Check our queries and assist the user as appropriate
 	if (NVSDK_NGX_SUCCEED(resultUpdatedDriver))
 	{
 		if (needsUpdatedDriver)
 		{
-			// NVIDIA DLSS cannot be loaded due to outdated driver.
+			// If the end user needs to update the driver, check that our queries for the necessary driver version succeeded, then feed this info to the user
 			if (NVSDK_NGX_SUCCEED(resultMinDriverVersionMajor) && NVSDK_NGX_SUCCEED(resultMinDriverVersionMinor))
-			{
 				// Min Driver Version required: minDriverVersionMajor.minDriverVersionMinor
-				Utility::Printf("DLSS could not be loaded due to outdated driver, please upgrade to version %d", minDriverVersionMajor);
-			}
-			// Fallback to default AA solution (TAA etc)
-			Utility::Print("Falling back to default AA solution");
-
+				Utility::Printf("\nDLSS could not be loaded due to outdated driver, please upgrade to version %d.%d\n\n", minDriverVersionMajor, minDriverVersionMinor);
+			else 
+				// If we can't find the minimum driver required, print a generic update message atleast
+				Utility::Print("\nDLSS could not be loaded due to outdated driver, please upgrade to latest stable version\\nn");
 		}
-		else
-		{
-			// Driver update is not required
-		}
-	}
-
-	// Secondary hardware query
-	NVSDK_NGX_Result ResultDlssSupported = Params->Get(NVSDK_NGX_Parameter_SuperSampling_Available, &DLSS_Supported);
-	if (NVSDK_NGX_FAILED(ResultDlssSupported) || !DLSS_Supported)
-	{
-		Utility::Print("NVIDIA DLSS not available on this hardware");
+		// Else, a driver update is not required and we're good to go!
 	}
 
 	// Sometimes DLSS is denied due to API permissions
 	ResultDlssSupported = Params->Get(NVSDK_NGX_Parameter_SuperSampling_FeatureInitResult, &DLSS_Supported);
 	if (NVSDK_NGX_FAILED(ResultDlssSupported) || !DLSS_Supported)
-	{
-		Utility::Print("NVIDIA DLSS is denied for this solution");
-	}
+		Utility::Print("\nNVIDIA DLSS is denied for this solution\n\n");
+	else
+		// Final message to indicate success!
+		Utility::Print("\nNVIDIA DLSS is supported!\n\n");
+
 
 	// Destroy parameter map
 	NVSDK_NGX_D3D12_DestroyParameters(Params);
